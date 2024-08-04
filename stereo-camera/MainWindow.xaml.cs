d17ddd568ba1c7817f3938905f466a77358da8bd
xufenyi <xufenyi@outlook.com>
2024-08-04 22:47:51 +0800

    Create GUI

---
+++ stereo-camera/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace stereo_camera;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
